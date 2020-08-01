03479fa329510cd9c24733da8f9cc1c5f0937e95
dhruvstan <50947002+dhruvstan@users.noreply.github.com>
2020-08-01 20:53:41 +0530

    added community forum and specific help pages

--- Frontend/src/Main.js
+++ Frontend/src/Main.js
@@ -7,22 +7,25 @@ import { createBrowserHistory } from "history";
 import SchemeCentral from "./views/Schemes/Scheme";
 import SchemeView from "./views/Schemes/SchemeView";
 import Contact from "./views/contact";
-
+import Help from "./views/community";
+import SpecificHelp from "./views/help";
 
 const history = createBrowserHistory();
 
 const Main = () => {
-    return (
-        <div>
-            <Switch history={history}>
-                <Route path="/signin" component={Signin} />{" "}
-                <PrivateRoute path="/contact" component={Contact} />{" "}
-                <PrivateRoute path="/dashboard" component={Dashboard} />{" "}
-                <Route path="/schemes" component={SchemeView} />
-                <Route path="/scheme-details/:slug" component={SchemeCentral} />{" "}
-            </Switch>{" "}
-        </div>
-    );
+  return (
+    <div>
+      <Switch history={history}>
+        <Route path="/signin" component={Signin} />{" "}
+        <PrivateRoute path="/contact" component={Contact} />{" "}
+        <PrivateRoute path="/help" component={Help} />{" "}
+        <PrivateRoute path="/dashboard" component={Dashboard} />{" "}
+        <Route path="/schemes" component={SchemeView} />
+        <Route path="/shelp/" component={SpecificHelp} />
+        <Route path="/scheme-details/:slug" component={SchemeCentral} />{" "}
+      </Switch>{" "}
+    </div>
+  );
 };
 
 export default Main;
---
+++ Frontend/src/views/community.js
@@ -0,0 +1,245 @@
+import React from "react";
+import { createMuiTheme } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+
+import { green } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
+});
+const currencies = [
+  {
+    value: "USD",
+    label: "option 1",
+  },
+  {
+    value: "EUR",
+    label: "option 2",
+  },
+  {
+    value: "BTC",
+    label: "option 3",
+  },
+];
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
+  },
+  heading: {
+    color: "#E76829",
+    fontWeight: "600",
+    display: "flex",
+    justifyItems: "left",
+    paddingTop: "1rem",
+  },
+  input: {
+    display: "none",
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  my_button: {
+    margin: theme.spacing(1, 1.5),
+    float: "right",
+  },
+  my_form: {
+    display: "flex",
+    flexDirection: "row",
+    justifyItems: "left",
+    paddingBottom: "10px",
+  },
+}));
+
+export default function Support() {
+  const classes = useStyles();
+  const [currency, setCurrency] = React.useState("EUR");
+  const [value, setValue] = React.useState("Controlled");
+
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <section
+        style={{
+          paddingLeft: "10%",
+          paddingTop: "2rem",
+          paddingBottom: "2rem",
+        }}
+      >
+        {/* Hero unit */}
+        <Container>
+          <div>
+            <Typography
+              component="div"
+              style={{
+                fontWeight: "600",
+                marginTop: "3rem",
+                width: "90vh",
+                height: "7vh",
+                color: "black",
+                borderBottom: `1px solid ${theme.palette.divider}`,
+                marginBottom: "1rem",
+              }}
+              variant="h4"
+              align="left"
+              gutterBottom
+            >
+              Get help with Nidhi Sanchar
+            </Typography>
+
+            <form>
+              <h6 className={classes.heading}>From</h6>
+              <div className={classes.my_form}>
+                <TextField
+                  style={{
+                    width: "40%",
+                  }}
+                  id="outlined-select-currency-native-size-small"
+                  select
+                  label=""
+                  value={currency}
+                  onChange={handleChange}
+                  size="small"
+                  SelectProps={{
+                    native: true,
+                  }}
+                  align="left"
+                  variant="outlined"
+                >
+                  {currencies.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+              </div>
+              <h6 className={classes.heading}>Account or Organization</h6>
+              <div className={classes.my_form}>
+                <TextField
+                  style={{
+                    width: "40%",
+                  }}
+                  id="outlined-select-currency-native-size-small"
+                  select
+                  label=""
+                  size="small"
+                  value={currency}
+                  onChange={handleChange}
+                  SelectProps={{
+                    native: true,
+                  }}
+                  variant="outlined"
+                >
+                  {currencies.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+              </div>
+              <h6 className={classes.heading}>Subject</h6>
+              <div className={classes.my_form}>
+                <TextField
+                  style={{
+                    width: "40%",
+                  }}
+                  label=""
+                  id="outlined-size-small"
+                  defaultValue=""
+                  variant="outlined"
+                  size="small"
+                />
+              </div>
+
+              <h6 className={classes.heading}>How Can we help ?</h6>
+
+              <div className={classes.my_form}>
+                <TextField
+                  style={{
+                    width: "70%",
+                  }}
+                  id="outlined-multiline-static"
+                  multiline
+                  rows={8}
+                  placeholder="Please tell us what exactly you want to accomplish"
+                  variant="outlined"
+                />
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyItems: "left",
+                  paddingTop: "20px",
+                }}
+              >
+                <div style={{ paddingRight: "20px" }}>
+                  <label htmlFor="contained-button-file">
+                    <Button
+                      variant="contained"
+                      color="default"
+                      component="span"
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyItems: "left",
+                      }}
+                    >
+                      Upload a screenshot or Doc
+                    </Button>
+                  </label>
+                </div>
+                <div>
+                  <input
+                    style={{
+                      display: "flex",
+                      height: "5vh",
+                    }}
+                    accept="image/file*"
+                    id="contained-button-file"
+                    multiple
+                    type="file"
+                  />
+                </div>
+              </div>
+              <div
+                style={{
+                  paddingTop: "25px",
+                }}
+              >
+                <Button
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyItems: "left",
+                    backgroundColor: "#E76829",
+                    color: "white",
+                  }}
+                  variant="contained"
+                  className={classes.button}
+                >
+                  Send Request
+                </Button>
+              </div>
+            </form>
+          </div>
+        </Container>
+      </section>
+    </React.Fragment>
+  );
+}
