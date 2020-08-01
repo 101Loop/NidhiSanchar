56a4a3e94d0717112eecc16106d25db3f3a7abe1
akhsri <43913678+akhsri@users.noreply.github.com>
2020-08-01 20:44:16 +0530

    Added dashboard page (#20)

---
+++ Frontend/src/views/Dashboard.js
@@ -0,0 +1,162 @@
+import React, { Component, Fragment } from "react";
+import NotificationSideBar from "../components/NotificationSideBar";
+import SchemeCard from "../components/SchemesCard";
+import CustomCard from "../components/CustomCard";
+import { getUserInfo } from "../services/auth";
+
+class Dashboard extends Component {
+  componentDidMount() {
+    getUserInfo();
+  }
+  render() {
+    const ListItems = [
+      {
+        department: "Agriculture",
+        date: "27/05/2021",
+        description: `Adding to its digital troubles is Japan's vertically structured
+          bureaucracy: each ministry as well as local governments, for instance,
+        have developed`,
+        name: "Green revolution",
+      },
+      {
+        department: "Agriculture",
+        date: "27/05/2021",
+        description: `Adding to its digital troubles is Japan's vertically structured
+          bureaucracy: each ministry as well as local governments, for instance,
+        have developed`,
+        name: "Green revolution",
+      },
+      {
+        department: "Agriculture",
+        date: "27/05/2021",
+        description: `Adding to its digital troubles is Japan's vertically structured
+          bureaucracy: each ministry as well as local governments, for instance,
+        have developed`,
+        name: "Green revolution",
+      },
+      {
+        department: "Agriculture",
+        date: "27/05/2021",
+        description: `Adding to its digital troubles is Japan's vertically structured
+          bureaucracy: each ministry as well as local governments, for instance,
+        have developed`,
+        name: "Green revolution",
+      },
+      {
+        department: "Agriculture",
+        date: "27/05/2021",
+        description: `Adding to its digital troubles is Japan's vertically structured
+          bureaucracy: each ministry as well as local governments, for instance,
+        have developed`,
+        name: "Green revolution",
+      },
+      {
+        department: "Agriculture",
+        date: "27/05/2021",
+        description: `Adding to its digital troubles is Japan's vertically structured
+          bureaucracy: each ministry as well as local governments, for instance,
+        have developed`,
+        name: "Green revolution",
+      },
+    ];
+    const CardInfoItems = [
+      {
+        name: "Schemes created",
+        num: "15",
+      },
+      {
+        name: "Requests Pending",
+        num: "24",
+      },
+      {
+        name: "Requests Closed",
+        num: "5",
+      },
+      {
+        name: "Discussions Open",
+        num: "50",
+      },
+    ];
+    return (
+      <div
+        className="dashboard"
+        style={{ overflow: "hidden", backgroundColor: "#F7F8F9" }}
+      >
+        <Fragment>
+          <div className="row">
+            <div className="col-12 col-md-1"></div>
+
+            <div
+              className="col-12 col-md-7"
+              style={{ margin: "3rem 1rem 1rem 1rem" }}
+            >
+              <div className="status-card" style={{ margin: "2rem 0" }}>
+                <div className="container">
+                  <div className="row">
+                    {CardInfoItems.map((item) => (
+                      <CustomCard name={item.name} num={item.num} />
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div
+                style={{
+                  margin: "1rem 5rem 0 0",
+                  padding: "1rem 0 0 3rem",
+                  fontWeight: "600",
+                  textAlign: "start",
+                }}
+              >
+                <h3
+                  style={{
+                    fontSize: "32px",
+                    padding: "1rem 2rem 0 0",
+                    fontWeight: "600",
+                    // color: "#E76829",
+                    textAlign: "initial",
+                  }}
+                >
+                  Latest Updates
+                </h3>
+              </div>
+
+              <div
+                className="list"
+                style={{
+                  maxHeight: "56vh",
+                  overflowY: "scroll",
+                }}
+              >
+                {ListItems.map((item) => (
+                  <div className="scheme-list-items">
+                    <SchemeCard
+                      name={item.name}
+                      date={item.date}
+                      department={item.department}
+                      description={item.description}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div
+              style={{ margin: "3rem 1rem 1rem 1rem" }}
+              className=" row right-sidebar"
+            >
+              <NotificationSideBar />
+            </div>
+          </div>
+          <style>
+            {`.row {
+                       display: flex;
+                       align-items: center;
+              }
+              `}
+          </style>
+        </Fragment>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
