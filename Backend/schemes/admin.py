f6ffe0bd663c9a2d4def67027360ebb4853d6e0b
Sumit Singh <sumit.singh4613@gmail.com>
2020-08-02 13:08:10 +0530

    adds scheme fund request for states (#32)

--- Backend/schemes/admin.py
+++ Backend/schemes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from schemes.models import Scheme, FundRequest
+from schemes.models import Scheme, FundRequest, SchemeFundRequest
 
 
 @admin.register(Scheme)
@@ -16,4 +16,12 @@ class FundRequestAdmin(admin.ModelAdmin):
     list_display = ("id", "scheme", "created_by", "status")
     list_filter = ("status", "scheme", "created_by")
     search_fields = ("scheme__name",)
+    readonly_fields = ["date_created", "date_updated"]
+
+
+@admin.register(SchemeFundRequest)
+class SchemeFundRequestAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "date_of_launching", "funds_required", "status")
+    list_filter = ("status",)
+    search_fields = ("name",)
     readonly_fields = ["date_created", "date_updated"]
