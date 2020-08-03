a14e9d7c6868d4dff921f0a0fc10aabed9e54209
Harsh Choudhary <harsh.hkc1006@gmail.com>
2020-08-03 10:31:06 +0530

    Harsh changes (#50)
    * Fixed positioning of captcha box
    
    * harsh-changes
    
    * Added profile page

---
+++ Frontend/src/views/profile/ProfileView.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Modi from "../../assets/pmmodi.jpg";
+import MyProfile from "./MyProfile";
+
+class ProfileView extends Component {
+  details = [
+    {
+      name: "Harsh",
+      birthday: "13/08/1999",
+      gender: "male",
+      isStateUser: "True",
+      department: "Technology",
+      photo: { Modi },
+    },
+  ];
+  render() {
+    return (
+      <div className="profile">
+        <div className="profile-heading">
+          <h2>My Profile</h2>
+          <p style={{ color: "#5F6368" }}>
+            Basic info, like your name and photo, that you use on Nidhi Sanchar
+          </p>
+        </div>
+        <div className="profile-pic">
+          <img src={Modi} alt="" className="profile-img"></img>
+        </div>
+        <div className="personal">
+          <MyProfile
+            name={"Harsh"}
+            birthday={"13/08/1999"}
+            gender={"Male"}
+            isStateUser={"True"}
+            department={"Education"}
+            phone={8368991155}
+            email={"harsh@gmail.com"}
+            username={"harshkc"}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProfileView;
