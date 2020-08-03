d4715144d21a8e500a81aaa5352593d50aff4810
Sumit Singh <sumit.singh4613@gmail.com>
2020-08-03 12:27:44 +0530

    Adds Hindi Translation for Admin Panel (#56)
    * adds language translation for admin
    
    * translates admin panel to hindi
    
    * adds django-rosetta

--- Backend/department/apps.py
+++ Backend/department/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class DepartmentConfig(AppConfig):
     name = "department"
+    verbose_name = _("Department")
